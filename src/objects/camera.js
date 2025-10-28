2b5de504b2c4b2575b1bf7828e82352210dd078d
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2025-10-28 15:56:25 +0330

    refactor application

--- src/objects/camera.js
+++ src/objects/camera.js
@@ -4,11 +4,14 @@ const windowInnerWidth = isMobile ? window.innerWidth : window.innerWidth / 2
 
 
 const camera = new three.PerspectiveCamera(
-    100,
+    65,
     (windowInnerWidth) / window.innerHeight,
     0.01,
     1000
 )
+
+camera.position.x = 40
+camera.position.y = 15
 camera.position.z = isMobile ? 40 : 30
 
 export { camera }
